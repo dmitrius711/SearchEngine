6468461b5441a12e3c77f01a650c7b04ad813a9d
dmitriy711 <137221759+dmitrius711@users.noreply.github.com>
2025-02-06 21:14:31 +0300

    Update ConverterJSON.h

--- include/ConverterJSON.h
+++ include/ConverterJSON.h
@@ -23,11 +23,6 @@ public:
     *  количества ответов на один запрос
     * @return
     */
-    int GetResponsesLimit();
-    /**
-    * Метод получения запросов из файла requests.json
-    * @return возвращает список запросов из файла requests.json
-    */
     std::vector<std::string> GetRequests();
     /**
     * Положить в файл answers.json результаты поисковых запросов
